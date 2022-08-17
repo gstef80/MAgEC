f606fa00e64a7003b9258b204f9ef35419b24a2a
Rajas Kale <rajas.p.kale@gmail.com>
2022-08-16 19:07:49 -0700

    Agg (#6)
    Co-authored-by: Rajas Kale <rajas.kale@anthem.com>

--- src/table15/runner.py
+++ src/table15/runner.py
@@ -23,8 +23,12 @@ def run(configs_path='../configs/pima_diabetes.yaml'):
     features = plutils.get_from_configs(configs, 'FEATURES')
     models = plutils.get_from_configs(configs, 'MODELS')
 
+    # Format check for Yaml configs
     if baselines is None:
         baselines = [None]
+
+    features = sorted(features)
+    # baselines = sorted(baselines)
 
     pima, x_train, x_validation, stsc, x_train_p, x_validation_p, y_train_p, y_validation_p = pm.pima_data(configs)
     print(x_train_p.shape)
@@ -75,7 +79,7 @@ def run(configs_path='../configs/pima_diabetes.yaml'):
             baseline_runs[baseline].append(run_dfs[key])
         
         # Def this process:
-        baseline_scores = {}
+        baseline_to_scores_df = {}
         all_joined = {}
         for baseline, model_runs in baseline_runs.items():
             baseline_joined = mg.magec_models(*model_runs,
@@ -83,12 +87,46 @@ def run(configs_path='../configs/pima_diabetes.yaml'):
                                 Ydata=y_validation_p,
                                 features=features)
             baseline_ranked_df = mg.magec_rank(baseline_joined, rank=len(features), features=features, models=models)
-            baseline_scores_df = agg_scores(baseline_ranked_df, policy=policy, models=models)
+            scores_df = agg_scores(baseline_ranked_df, policy=policy, models=models)
 
             all_joined[baseline] = baseline_joined
-            baseline_scores[baseline] = baseline_scores_df
+            baseline_to_scores_df[baseline] = scores_df
 
-        return baseline_scores, all_joined
+    output_logits = {}
+    output_probs = {}
+
+    # TODO: fix baselines upstream  to handle None as 0
+    if None in baselines:
+        idx = baselines.index(None)
+        baselines[idx] = 0
+
+    for baseline in baselines:
+        if baseline is None:
+            baseline = 0
+        df_logits = pd.DataFrame.from_records(baseline_to_scores_df[baseline]['logits'])
+        base_logits_strings = get_string_repr(df_logits, features)
+        output_logits[baseline] = base_logits_strings
+
+        df_probs = pd.DataFrame.from_records(baseline_to_scores_df[baseline]['probs'])
+        base_probs_strings = get_string_repr(df_probs, features)
+        output_probs[baseline] = base_probs_strings
+    
+    df_logits_out = pd.DataFrame.from_records(output_logits)
+    df_logits_out['feature'] = features
+    # re-order cols
+    cols = ['feature'] + baselines
+    df_logits_out[cols]
+
+    df_probs_out = pd.DataFrame.from_records(output_probs)
+    df_probs_out['feature'] = features
+    # re-order cols
+    cols = ['feature'] + baselines
+    df_probs_out[cols]
+
+    print(df_logits_out.head())
+    print(df_probs_out.head())
+
+    return (df_logits_out, df_probs_out), all_joined
 
 
 def agg_scores(ranked_df, policy='mean', models=('mlp', 'rf', 'lr')):
@@ -101,7 +139,7 @@ def agg_scores(ranked_df, policy='mean', models=('mlp', 'rf', 'lr')):
     
     return pd.DataFrame.from_records(out)
 
-def     run_magecs(return_dict, clf, x_validation_p, y_validation_p, model_name, baseline=None, features=None):
+def run_magecs(return_dict, clf, x_validation_p, y_validation_p, model_name, baseline=None, features=None):
     print(model_name)
     p_name = mp.current_process().name
     print('Starting:', p_name)
@@ -115,3 +153,13 @@ def     run_magecs(return_dict, clf, x_validation_p, y_validation_p, model_name,
     magecs = magecs.merge(y_validation_p, left_on=['case', 'timepoint'], right_index=True)
     print('Exiting :', p_name)
     return_dict[p_name] = magecs
+
+
+def get_string_repr(df, feats):
+    base_strings = []
+    for feat in feats:
+        mean = round(df[feat].mean(), 4)
+        std = round(df[feat].std(), 4)
+        string_repr = f'{mean} +/- {std}'
+        base_strings.append(string_repr)
+    return base_strings
